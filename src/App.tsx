17513cb2fe04d52f2254111890f19270afab0e1e
Christian Sachs <byaskew3@gmail.com>
2023-12-06 17:46:29 -0700

    Managing Forms & Posting to Backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import Cart from "./Cart/Cart"
-import Games from "./Games/Games"
-import { useState } from 'react'
-import Nav from "./Nav/Nav"
+import Form from "./Form/Form"
 
 const App = () => {
 
-  const [cartItems, setCartItems] = useState(['coffee mug', 'jersey', 'camera'])
-
-  const handleClick = () => {
-    setCartItems([])
-  }
-
   return (
     <>
-      <Nav cartItemsLength={cartItems.length}/>
-      <Cart cartItems={cartItems} handleClick={handleClick}/>
+      <Form/>
     </>
   )
 }
